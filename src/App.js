c0609d24497d4a4f3eb65eb8df45c48f9b48172c
Piyush Singh <1806397@kiit.ac.in>
2022-12-20 11:53:15 +0530

    4th commit:Added navlink and dynamic routes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import { Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
 function App() {
   return (
     <>
       <MainHeader />
-      <div>
+      <main>
         <Route path="/welcome">
           <Welcome />
         </Route>
         <Route path="/products">
           <Products />
         </Route>
-      </div>
+        <Route path="/product-detail/:productId">
+          <ProductDetail />
+        </Route>
+      </main>
     </>
   );
 }
